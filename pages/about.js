dc322388c6ecc3b93514b414f040180c93a4e2f0
takara_shinya　[Administrator] <takara_shinya@ca-adv.co.jp>
2021-09-01 16:46:20 +0900

    add hello and about pages

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const About = () => {
+  return <>
+    <div>about page</div>
+    <Link href='/'>Home</Link>
+  </>
+};
+
+export default About;
+
