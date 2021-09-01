96331b8a938bfe7cfc9fb875f778ed00e86001b4
takara_shinya　[Administrator] <takara_shinya@ca-adv.co.jp>
2021-09-01 16:51:04 +0900

    add perple button

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
+import Button from '@material-ui/core/Button';
 
 const About = () => {
   return <>
     <div>about page</div>
     <Link href='/'>Home</Link>
+      <Button variant="contained" color="primary">
+        Hello hogehoge!
+      </Button>
   </>
 };
 
